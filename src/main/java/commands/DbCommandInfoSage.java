73eb75f0223c3e624675a96e200f6fca0cf76381
Milkyway Architect <massandharsh@gmail.com>
2025-06-25 17:16:43 +0530

    Removing non useful log statements
    Signed-off-by: Milkyway Architect <massandharsh@gmail.com>

--- src/main/java/commands/DbCommandInfoSage.java
+++ src/main/java/commands/DbCommandInfoSage.java
@@ -20,7 +20,7 @@ public class DbCommandInfoSage implements Commands<String>{
         if(!verifyCommand(command)){
             throw new RuntimeException("Something went wrong with executed command");
         }
-        //New feature introduced where unesessdccary sout is removed with speeling fix
+        //why this feature is bad and not required ngl
         String [] commands = commandExtractor(command);
         try(FileInputStream fis = new FileInputStream(commands[0])){
             //First we have to skip the 16 bytes of this file as that is header
